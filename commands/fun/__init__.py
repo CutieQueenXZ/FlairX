a87760ad78ef39f4c2233ccfada8c217d6d3baaf
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-12 11:59:31 +0300

    Update __init__.py

--- commands/fun/__init__.py
+++ commands/fun/__init__.py
@@ -1,5 +1,5 @@
-from . import joke, fact, hug, coinflip, eightball
+from . import joke, fact, hug, coinflip, eightball, meme
 
 def handle_fun_command(comment):
-    for module in [joke, fact, hug, coinflip, eightball]:
+    for module in [joke, fact, hug, coinflip, eightball, meme]:
         module.handle(comment)
