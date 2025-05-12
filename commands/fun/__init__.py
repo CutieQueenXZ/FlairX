574e11cc2962d04beda32dbb736e26ab38aabc6e
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-12 22:17:10 +0300

    Update __init__.py

--- commands/fun/__init__.py
+++ commands/fun/__init__.py
@@ -2,7 +2,7 @@ from . import joke, fact, hug, coinflip, eightball, meme, rizz
 
 COMMAND_MODULES = [joke, fact, hug, coinflip, eightball, meme, rizz]
 
-def handle_fun_command(comment):
+def handle_commands(comment):
     body = comment.body.lower()
     for module in COMMAND_MODULES:
         if hasattr(module, "handle"):
