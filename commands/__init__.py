3bc7e421a690ef7e6a7cbcd636dde3ba3a3a47bd
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-14 00:31:04 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,4 +1,4 @@
-from commands.fun import joke, rizz, coinflip, eightball, hug, meme, bighug, ping, reverse, choose, motivate, compliment, insult
+from commands.fun import joke, rizz, coinflip, eightball, hug, meme, bighug, ping, reverse, choose, motivate, compliment, insult, dadjoke
 from commands.utility import help, website
 from commands.facts import catfacts, fact
 from commands.animals import cat, dog
@@ -6,7 +6,7 @@ from commands.basic import hello
 def handle_commands(comment):
     for module in [joke, rizz, fact, coinflip, eightball, hug, bighug, meme,
                    help, website, catfacts, cat, dog, hello, ping, reverse,
-                   choose, motivate, compliment, insult]:
+                   choose, motivate, compliment, insult, dadjoke]:
         try:
             module.handle(comment)
         except Exception as e:
