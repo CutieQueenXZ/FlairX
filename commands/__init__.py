f30bf40cdfea143cac57625262dcf5cf5de3a109
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-13 20:55:31 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,7 +2,7 @@ from commands.fun import joke, rizz, coinflip, eightball, hug, meme, bighug
 from commands.utility import help, website
 from commands.facts import catfacts, fact
 from commands.animals import cat, dog
-
+from commands.basic import hello
 def handle_commands(comment):
     for module in [joke, rizz, fact, coinflip, eightball, hug, bighug, meme,
                    help, website, catfacts, cat, dog]:
