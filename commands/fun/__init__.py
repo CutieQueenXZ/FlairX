28ec8c0be36e6fbc873f9329bf288cdc17f3cf3a
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-13 10:24:29 +0300

    Update __init__.py

--- commands/fun/__init__.py
+++ commands/fun/__init__.py
@@ -1,6 +1,6 @@
-from . import joke, fact, hug, coinflip, eightball, meme, rizz, catfacts, dog cat
+from . import joke, fact, hug, coinflip, eightball, meme, rizz, catfacts, dog, cat bighug,
 
-COMMAND_MODULES = [joke, fact, hug, coinflip, eightball, meme, rizz, catfacts, dog cat]
+COMMAND_MODULES = [joke, fact, hug, coinflip, eightball, meme, rizz, catfacts, dog, cat, bighug]
 
 def handle_commands(comment):
     body = comment.body.lower()
