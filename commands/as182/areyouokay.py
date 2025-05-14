70ee6a55fa1a2dfbce2c1de586493b7f0e5a3bce
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-15 02:47:15 +0300

    Update areyouokay.py

--- commands/as182/areyouokay.py
+++ commands/as182/areyouokay.py
@@ -1,3 +1,3 @@
 def handle(comment):
     if comment.body.lower().startswith("!areyouokay"):
-        comment.reply("o-of course i am! why did you say that? (read it opposite, next up 'really-dddd' put it into '!' !really")
+        comment.reply("o-of course i am! why did you say that? (read it opposite, next up 'really-725' put it into '!' !really")
