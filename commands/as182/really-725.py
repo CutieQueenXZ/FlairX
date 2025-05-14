a3379644a2c8604c7dce58631c06f6611e5b5334
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-15 02:17:30 +0300

    Create really-725.py

---
+++ commands/as182/really-725.py
@@ -0,0 +1,3 @@
+def handle(comment):
+    if comment.body.lower().startswith("!really-725"):
+        comment.reply("okay, you... got me there... my family is healthy, they're not in hospital, i know my day is getting better, i have money, i love myself, and then, you can reply to me! i love myself! (read as opposite.)")
