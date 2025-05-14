0f72c39c1d3aa37b140cbb0f661edafae6989456
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-15 02:18:51 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,12 +4,13 @@ from commands.facts import catfacts, fact
 from commands.animals import cat, dog, birb, frog, duck
 from commands.basic import hello, define, quote, whoami
 from commands.ai import gemini
+from commands.as182 import areyouokay, really-725
 def handle_commands(comment):
     for module in [joke, rizz, fact, coinflip, eightball, hug, bighug, meme,
                    help, website, catfacts, cat, dog, hello, ping, reverse,
                    choose, motivate, compliment, insult, dadjoke, birb, frog, duck,
                    weather, truth, dare, gemini, fliptext, howgay, ship, quote,
-                   whoami, uwuify]:
+                   whoami, uwuify, areyouokay, really-725]:
         try:
             module.handle(comment)
         except Exception as e:
