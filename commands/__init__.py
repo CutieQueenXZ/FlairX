efb1ad97f1451af76f5d758fb54057af8b6dfc46
Yayz! :o <149315213+CutieQueenXZ@users.noreply.github.com>
2025-05-14 18:09:31 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -3,7 +3,6 @@ from commands.utility import help, website, weather
 from commands.facts import catfacts, fact
 from commands.animals import cat, dog, birb, frog, duck
 from commands.basic import hello
-from commands.ai import 
 def handle_commands(comment):
     for module in [joke, rizz, fact, coinflip, eightball, hug, bighug, meme,
                    help, website, catfacts, cat, dog, hello, ping, reverse,
